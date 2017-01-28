2dfb37f7c5d810bd7c64df09348a7fa947fb791b
Adam Some <adamsome@gmail.com>
2017-01-27 19:23:42 -0500

    Add axios base API URL

--- app/api/index.js
+++ app/api/index.js
@@ -1,20 +1,21 @@
 import axios from 'axios'
 
-const serverUrl    = 'http://localhost:3000'
-const apiPath      = `${serverUrl}/api/v1`
+const request = axios.create({
+  baseURL: 'http://localhost:3000/api/v1'
+})
 
 export const fetchUser = (userID) => (
-  axios.get(`${apiPath}/users/${userID}`)
+  request.get(`/users/${userID}`)
     .then((res) => res.data)
 )
 
 export const fetchUserLists = (userID) => (
-  axios.get(`${apiPath}/users/${userID}/lists`)
+  request.get(`/users/${userID}/lists`)
     .then((res) => res.data)
 )
 
 export const addList = (userID, name) => (
-  axios.post(`${apiPath}/users/${userID}/lists`, { name })
+  request.post(`/users/${userID}/lists`, { name })
     .then((res) => res.data)
 )
 
