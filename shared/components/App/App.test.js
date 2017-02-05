485cec4369380ab92055a39f37fb1f6b21f4ea3d
Adam Some <adamsome@gmail.com>
2017-02-04 21:36:56 -0500

    Update jest config

---
+++ shared/components/App/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+
+import App from './App'
+
+let wrapper
+beforeEach(() => {
+  wrapper = shallow(<App />)
+})
+
+it('renders correctly', () => {
+  expect(wrapper).toMatchSnapshot()
+})
